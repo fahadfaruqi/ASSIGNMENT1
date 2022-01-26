74e53902b1eeb71f29e716dbd12bbfeda4eb94f5
fahadfaruqi <92307041+fahadfaruqi@users.noreply.github.com>
2022-01-26 12:55:12 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,7 @@
+var num = 10;
+
+if (num >= 10) {
+  console.log("Number is greater than 10");
+} else {
+  console.log("Number is not greater than 10");
+}
---
+++ task2.js
@@ -0,0 +1,11 @@
+var num1 = 150;
+var num2 = 200;
+var num3 = 50;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log(num2);
+} else {
+  console.log(num3);
+}
